8ed5038bfbf1a42c9683dab4a86368842956603f
hassan <hassanibrahimndahiro@gmail.com>
2024-11-06 19:00:19 +0300

    react programm

--- src/Mycomponent.jsx
+++ src/Mycomponent.jsx
@@ -26,22 +26,26 @@ const isEmployedStatus = () => {
     setEmployed(!employed);
    
 }
+const resetAge = () => {
+    setAge(0);
+}
 
 
 
 return (
     <div>
-      <p>This is a simple React component.</p>
-      <p>Name : {name} </p>
-      <button onClick={updateName}onPointerOver={updateName}>setName</button>
-      <button onClick={removeName}onPointerOver={removeName}>removeName</button>
+      <p className='display'>This is a simple React component.</p>
+      <p className='display'>Name : {name} </p>
+      <button className='button'onClick={updateName}>setName</button>
+      <button className='button'onClick={removeName}>removeName</button>
 
-      <p>Age : {age} </p>
-      <button onClick={incrementAge}onPointerOver={incrementAge}>IncrementAge</button>
-      <button onClick={decrementAge}onPointerOver={decrementAge}>decrementAge</button>
+      <p className='display'>Age : {age} </p>
+      <button className='button' onClick={incrementAge}>IncrementAge</button>
+   <button className='button' onClick={resetAge}>resetAge</button>   <button className='button' onClick={decrementAge}>decrementAge</button>
+      
 
-      <p>isEmployed : {employed  ? "Yes" : "No"} </p>
-      <button onClick={isEmployedStatus} onPointerOver={isEmployedStatus}>Toggle status</button>
+      <p className='display'>isEmployed : {employed  ? "Yes" : "No"} </p>
+      <button className='button' onClick={isEmployedStatus} >Toggle status</button>
     </div> 
 );
 }
